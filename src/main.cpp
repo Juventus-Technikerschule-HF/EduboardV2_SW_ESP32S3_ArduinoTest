76a77ec0f35224e2a0294aeba8c268b92d9b3520
Martin Burger <mburger@kochag.ch>
2023-07-13 11:13:55 +0200

    test touch controller ft6236

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <WIRE.h>
 #include <Arduino_GFX_Library.h>
 //#include <FFat.h>
 //#include <LittleFS.h>
@@ -6,6 +7,7 @@
 //#include <SD.h>
 //#include <SD_MMC.h>
 #include "JpegFunc.h"
+#include "focaltech.h"
 
 #define GFX_BL   -1 // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
 #define PIN_RST  8
@@ -23,6 +25,8 @@ Arduino_DataBus *bus = new Arduino_ESP32SPI(PIN_DC, PIN_CS, PIN_SCK, PIN_MOSI, P
 /* More display class: https://github.com/moononournation/Arduino_GFX/wiki/Display-Class */
 //Arduino_GFX *gfx = new Arduino_ILI9488(bus, PIN_RST, 3 /* rotation */, false /* IPS */);
 Arduino_GFX *gfx = new Arduino_ILI9488_18bit(bus, PIN_RST /* RST */, 1 /* rotation */, false /* IPS */);
+
+FocalTech_Class touch;
 
 static int jpegDrawCallback(JPEGDRAW *pDraw)
 {
@@ -65,19 +69,40 @@ void setup() {
              0 /* x */, 0 /* y */, gfx->width() /* widthLimit */, gfx->height() /* heightLimit */);
     Serial.printf("Time used: %lu\n", millis() - start);
   }
-  Serial.printf("End Setup");
+  Wire.begin(2, 1, 400000);
+  Serial.println("\nStart Touch:\n");
+  Serial.printf("Touch Init: %i\n", touch.begin(Wire, FOCALTECH_SLAVE_ADDRESS));
+  Serial.printf("Touch MonitorTime: %i", touch.getMonitorTime());
+  touch.setPowerMode(FOCALTECH_PMODE_ACTIVE);
+  touch.disableINT();
+
+  Serial.println("End Setup\n");
+
+
 
   delay(5000);
 }
 
 void loop() {
-  Serial.printf("Start Loop");
-  int w = gfx->width();
-  int h = gfx->height();
+  //Serial.printf("Start Loop");
+  // int w = gfx->width();
+  // int h = gfx->height();
 
-  unsigned long start = millis();
-  jpegDraw(JPEG_FILENAME, jpegDrawCallback, true /* useBigEndian */, 0, 0, w, h);
-  Serial.printf("Time used: %lu\n", millis() - start);
-
-  delay(1000);
+  // unsigned long start = millis();
+  // jpegDraw(JPEG_FILENAME, jpegDrawCallback, true /* useBigEndian */, 0, 0, w, h);
+  // //Serial.printf("Time used: %lu\n", millis() - start);
+  // Serial.printf("Total heap: %d\n", ESP.getHeapSize());
+  // Serial.printf("Free heap: %d\n", ESP.getFreeHeap());
+  // Serial.printf("Total PSRAM: %d\n", ESP.getPsramSize());
+  // Serial.printf("Free PSRAM: %d\n", ESP.getFreePsram());
+  // delay(1000);
+  uint16_t x,y;
+  touch.getPoint(x,y);
+  uint8_t touched = touch.getTouched();  
+  if(touched > 0) {    
+    Serial.printf("Touched:%i\n>x:%i\n>y:%i\n", touched, x,y);
+  } else {
+    Serial.printf("Touched:%i\n", touched);
+  }
+  delay(10);
 }
